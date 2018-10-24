69cdb19c46d9c5056411478fb06cadc8ebf04429
Alexey Vadimovich <31240690+Avb-89@users.noreply.github.com>
2018-10-24 20:25:10 +0400

    error: syntax error, unexpected '=', at /etc/nixos/configuration.nix:36:31

--- Hyper-V/configuration_V1.nix
+++ Hyper-V/configuration_V1.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -10,99 +6,35 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-#Bios Install
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.version = 2;
-  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+# Variables_installation
 
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
+    # Bios
+      # boot.loader.grub.enable = true;
+      # boot.loader.grub.version = 2;
+      # boot.loader.grub.device = "/dev/sda";
 
-#EFI install
-   boot.loader.systemd-boot.enable = true;
-   boot.loader.efi.canTouchEfiVariables = true;
+    # EFI
+       boot.loader.systemd-boot.enable = true;
+       boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless
-    networking.hostName = "VMNixOS"; # support via wpa_supplicant.
-
-  # Select internationalisation properties.
-  # i18n = {
-  #   consoleFont = "Lat2-Terminus16";
-  #   consoleKeyMap = "us";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
-
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
-  time.timeZone = "Europe/Samara";
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-      wget vim htop mc screen git links2
-  ];
-
-  # services.parity.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.bash.enableCompletion = true;
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
+# LAN_Environment
   services.openssh.enable = true;
-  services.openssh.permitRootLogin = "yes";
-  # services.parity.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+# System_Settings
+  networking.hostName = "NixOS"; # support via wpa_supplicant.
+  time.timeZone = "Europe/Samara";
+  users.extraUsers.avb = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = ["wheel"];
+};
+  users.extraUsers.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWtDy2FB39MvQcMHHIKNyhnaTf73yeFar9dup2KxFjD avb-8@Sitis"
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+# Programms
+  environment.systemPackages = with pkgs;
+      [wget vim htop mc screen git links2];
 
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.extraUsers.guest = {
-  #   isNormalUser = true;
-  #   uid = 1000;
-  # };
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "17.09"; # Did you read the comment?
-
-  	users.extraUsers.avb = {
-  		isNormalUser = true;
-  		uid = 1000;
-  		extraGroups = [
-    	  	"wheel"
-  	   ];
-	};
+# Version
+  system.stateVersion = "17.09";
 }
