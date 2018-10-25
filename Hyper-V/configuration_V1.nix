e658c2601b655aa322b177588f5268fde5e0e3bc
Alexey Vadimovich <31240690+Avb-89@users.noreply.github.com>
2018-10-25 11:37:00 +0400

    Its Work

--- Hyper-V/configuration_V1.nix
+++ Hyper-V/configuration_V1.nix
@@ -13,28 +13,31 @@
       # boot.loader.grub.version = 2;
       # boot.loader.grub.device = "/dev/sda";
 
-    # EFI
-       boot.loader.systemd-boot.enable = true;
-       boot.loader.efi.canTouchEfiVariables = true;
-
-# LAN_Environment
-  services.openssh.enable = true;
-
-# System_Settings
-  networking.hostName = "NixOS"; # support via wpa_supplicant.
-  time.timeZone = "Europe/Samara";
-  users.extraUsers.avb = {
-    isNormalUser = true;
-    uid = 1000;
-    extraGroups = ["wheel"];
-};
-  users.extraUsers.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWtDy2FB39MvQcMHHIKNyhnaTf73yeFar9dup2KxFjD avb-8@Sitis"
+# EFI
+   boot.loader.systemd-boot.enable = true;
+   boot.loader.efi.canTouchEfiVariables = true;
 
 # Programms
-  environment.systemPackages = with pkgs;
-      [wget vim htop mc screen git links2];
+    environment.systemPackages = with pkgs;
+      [ wget vim htop mc screen git links2 ];
+
+
+# LAN_Environment
+    services.openssh.enable = true;
+    services.openssh.permitRootLogin = "yes";
+
+# System_Settings
+    networking.hostName = "NixOS"; # support via wpa_supplicant.
+    time.timeZone = "Europe/Samara";
 
 # Version
-  system.stateVersion = "17.09";
+    system.stateVersion = "17.09";
+# Users
+    users.extraUsers.avb = {
+      isNormalUser = true;
+      uid = 1000;
+      extraGroups = [ "wheel"  ];
+  };
+    users.extraUsers.root.openssh.authorizedKeys.keys =
+    [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWtDy2FB39MvQcMHHIKNyhnaTf73yeFar9dup2KxFjD avb-8@Sitis"  ];
 }
