ebeb296524b2ee8f7a29c228841f76a1e4d12cd8
Alexey Vadimovich <31240690+Avb-89@users.noreply.github.com>
2018-10-26 15:57:53 +0400

    work konfig

---
+++ Hyper-V/configuration_V2.nix
@@ -0,0 +1,45 @@
+# Конфиг для модификаций
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+# Variables_installation
+
+    # Bios
+      # boot.loader.grub.enable = true;
+      # boot.loader.grub.version = 2;
+      # boot.loader.grub.device = "/dev/sda";
+
+# EFI
+   boot.loader.systemd-boot.enable = true;
+   boot.loader.efi.canTouchEfiVariables = true;
+
+# Programms
+    environment.systemPackages = with pkgs;
+      [ wget vim htop mc screen git links2 ];
+
+
+# LAN_Environment
+    services.openssh.enable = true;
+    services.openssh.permitRootLogin = "yes";
+
+# System_Settings
+    networking.hostName = "NixOS"; # support via wpa_supplicant.
+    time.timeZone = "Europe/Samara";
+
+# Version
+    system.stateVersion = "17.09";
+# Users
+    users.extraUsers.avb = {
+      isNormalUser = true;
+      uid = 1000;
+      extraGroups = [ "wheel"  ];
+  };
+    users.extraUsers.root.openssh.authorizedKeys.keys =
+    [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWtDy2FB39MvQcMHHIKNyhnaTf73yeFar9dup2KxFjD avb-8@Sitis"  ];
+}
